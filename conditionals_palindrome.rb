b92899619f92aa0694fb27684ac9547732009ba7
osha7 <ogroetz@gmail.com>
2022-04-10 18:51:56 +0000

    tests pass

--- conditionals_palindrome.rb
+++ conditionals_palindrome.rb
@@ -16,3 +16,16 @@
 # true
 
 p "Enter one word"
+word_array = gets.chomp.split("")
+length = word_array.count
+half_array = length/2
+i = 0
+value = ""
+if (length < 2) 
+  value = false 
+end
+while (i < half_array)
+  value = word_array[i].downcase == word_array[-(i+1)].downcase
+  i = i + 1
+end 
+print value
--- conditionals_rps.rb
+++ conditionals_rps.rb
@@ -7,3 +7,13 @@
 # If you need a refresher on the rules of Rock, Paper, Scissors: https://en.wikipedia.org/wiki/Rock%E2%80%93paper%E2%80%93scissors
 
 p "Please choose rock, paper, or scissors:"
+ user_input = gets.chomp
+ p "You played #{user_input}!"
+ p "The computer played scissors!"
+ if user_input == "rock"
+  p 'You won!'
+ elsif user_input == "paper"
+  p 'You lost!'
+elsif user_input == "scissors"
+  p 'You tied!'
+ end
